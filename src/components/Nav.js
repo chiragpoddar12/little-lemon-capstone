1bba9c5866569593ccc19a191d20b14ca4a17caa
e122217 <chirag.poddar@mastercard.com>
2023-05-31 14:09:18 +0100

    Nav part 2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,38 @@
+import {useState} from "react";
+
 import "./../styles/styles.css"
 import logo from "../imgs/logo.png";
 import hamburger from "../imgs/hamburger.png";
+import cross from "../imgs/cross.png";
 function Nav() {
+
+    const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+    const onHamburgerButtonClick = () =>{
+        setIsMobileNavOpen(!isMobileNavOpen);
+    }
+
     return(
         <nav>
             <nav className={"hamburger-nav"}>
                 <a href={"/"}><img src={logo} alt={"Little Lemon Restaurant"} className={"logoImage"}/></a>
-                <button className={"hamburger-button"}>
-                    <img src={hamburger} alt={"Navigation Button"}/>
+                <button className={"hamburger-button"} onClick={onHamburgerButtonClick}>
+                    <img src={ isMobileNavOpen ? cross : hamburger } alt={"Navigation Button"}/>
                 </button>
             </nav>
-            <menu className={"navbar laptop"}>
+
+            {
+                isMobileNavOpen ?
+                    <menu className={"mobile-navbar"}>
+                        <a className={"navbar-button"} href={"/"}><h3>Home</h3></a>
+                        <a className={"navbar-button"} href={"/"}><h3>About</h3></a>
+                        <a className={"navbar-button"} href={"/"}><h3>Menu</h3></a>
+                        <a className={"navbar-button"} href={"/"}><h3>Reservations</h3></a>
+                        <a className={"navbar-button"} href={"/"}><h3>Order</h3></a>
+                        <a className={"navbar-button"} href={"/"}><h3>Login</h3></a>
+                    </menu> : <></>
+            }
+            <menu className={"laptop-navbar"}>
                 <a href={"/"}><img src={logo} alt={"Little Lemon Restaurant"} className={"logoImage"}/></a>
                 <a className={"navbar-button"} href={"/"}><h3>Home</h3></a>
                 <a className={"navbar-button"} href={"/"}><h3>About</h3></a>
